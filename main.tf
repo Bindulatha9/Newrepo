e840cf6b786629a4e66799d1629a7790bbdd1528
Bindulatha kondapalli <120655881+Bindulatha9@users.noreply.github.com>
2025-08-18 21:48:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
-resource "null_resource" "test" {}
+resource "aws_instance" "my_ec2" {
+    ami = "ami-0de716d6197524dd9"
+    instance_type = "t3.micro"
+    tags = {
+       Name = "myinstance"
+    }
+}
