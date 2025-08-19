625978e3d36fbb4165110631ae1e137c11e36536
Bindulatha kondapalli <120655881+Bindulatha9@users.noreply.github.com>
2025-08-19 21:05:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-      bucket = "terraform-backend-bucket9"
+      bucket = "terraform-backend-bucket99"
       key = "statefile/terraform.tfstate"
       region = "us-east-1"
       dynamodb_table = "Terraform-backend"
