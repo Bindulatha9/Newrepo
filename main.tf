345493795e246af90df213e4f7081b5d232f20fe
Bindulatha kondapalli <120655881+Bindulatha9@users.noreply.github.com>
2025-08-19 21:04:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
-    ami = "ami-0de716d6197524dd9"
-    instance_type = "t3.micro"
+    ami = "ami-00ca32bbc84273381"
+    instance_type = "t2.micro"
     tags = {
        Name = "myinstance"
     }
